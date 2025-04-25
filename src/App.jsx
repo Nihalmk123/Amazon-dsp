5ca4b6e85f9f47c2493a805b3882cec504140006
Nihalmk123 <mrnihal2589@gmail.com>
2025-04-25 19:45:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='mb-5' style={{color:'#E9762B'}}>Amazon DSP (Demand-Side Platform) </h1>
+    <h2>My Ordered List</h2>
+  <ol>
+    <li>First item</li>
+    <li>Second item</li>
+    <li>Third item</li>
+    <li>Fourth item</li>
+  </ol>
+    </>
+  )
+}
+
+export default App
